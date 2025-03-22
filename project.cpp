a4ae23f0e9be7f5192b2f542052e69f4b29fbc4d
alexgud <aleksejgudis623@gmail.com>
2025-03-22 18:05:38 +0300

    Message for greg

--- project.cpp
+++ project.cpp
@@ -3,5 +3,6 @@
 int main(){
     std::cout << "Hello from Alex Gudis";
     std::cout << "Hello from Greg Goozmer";
+    std::cout << "Greg Goozmer is going to fuck Dasha when I go home";
     return 0;
 }

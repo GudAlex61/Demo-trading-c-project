542cbed6cd79fd77619b500e73e504a59819c379
greg-goozmer <zherebenkoff170306@gmail.com>
2025-03-22 18:09:20 +0300

    <3

--- project.cpp
+++ project.cpp
@@ -4,5 +4,6 @@ int main(){
     std::cout << "Hello from Alex Gudis";
     std::cout << "Hello from Greg Goozmer";
     std::cout << "Greg Goozmer is going to fuck Dasha when I go home";
+    std::cout << "Ofc, Alexey. I very very want her... She is very beautiful!!! I love her.";
     return 0;
 }

bd7bf3fe89468270fb2d068068b5fd16188f82e8
alexgud <aleksejgudis623@gmail.com>
2025-03-23 19:54:53 +0300

    using position in main file

--- project.cpp
+++ project.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include "MakingAPosition.h"
 
 int main(){
     std::cout << "Hello from Alex Gudis";
     std::cout << "Hello from Greg Goozmer";
     std::cout << "Greg Goozmer is going to fuck Dasha when I go home";
     std::cout << "Ofc, Alexey. I very very want her... She is very beautiful!!! I love her.";
+
+    Positon position1 = MakingPosition();
+    int posShoulder = position1.getShoulder();
+    std::cout << posShoulder;
     return 0;
 }
